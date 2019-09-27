9064ced4a4619adb94279317f9f8e1f64cffb869
hzemzem <hajar.zemzem@gmail.com>
2019-09-27 14:53:10 -0400

    Updates

---
+++ precache-manifest.af5ebd311b6f11d724bcce09fbddeb13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c27d28275ae9e46a16ced8a01301f19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1327f92b203884bceec4",
+    "url": "/static/css/main.9e6b4575.chunk.css"
+  },
+  {
+    "revision": "41c00c020b933d573a5b",
+    "url": "/static/js/2.cbbdd573.chunk.js"
+  },
+  {
+    "revision": "1327f92b203884bceec4",
+    "url": "/static/js/main.05654d4b.chunk.js"
+  },
+  {
+    "revision": "37e0fe6f3399f7d0aef0",
+    "url": "/static/js/runtime~main.c6957241.js"
+  }
+]);
